53ea058df1b2152bc326262ebd001b2e925b671b
Efrén Campuzano <efrecamp@hotmail.com>
2025-02-13 11:20:51 -0700

    Arreglado diseño

--- iot_wiki/lib/iot_wiki.dart
+++ iot_wiki/lib/iot_wiki.dart
@@ -9,6 +9,7 @@ class IoTWiki extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: title,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
